26255eea243b1a4c21c09d55e11b1e4894666e3f
[Firat] <firat0705@hotmail.com>
2021-12-14 12:35:53 +0100

    andringar 10

--- betalning2/main.js
+++ betalning2/main.js
@@ -1,50 +1,18 @@
-$('form').card({
- 
-    // numara formatı
-    formatting: true,
-   
-    // form seçicileri
-    formSelectors: {
-      numberInput: 'input[name="number"]',
-      expiryInput: 'input[name="expiry"]',
-      cvcInput: 'input[name="cvc"]',
-      nameInput: 'input[name="name"]'
-    },
-    cardSelectors: {
-      cardContainer: '.jp-card-container',
-      card: '.jp-card',
-      numberDisplay: '.jp-card-number',
-      expiryDisplay: '.jp-card-expiry',
-      cvcDisplay: '.jp-card-cvc',
-      nameDisplay: '.jp-card-name'
-    },
-   
-    // mesajlar
-    messages: {
-      validDate: 'valid\nthru',
-      monthYear: 'month/year'
-    },
-   
-    // placeholder alanları
-    placeholders: {
-      number: '&bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull;',
-      cvc: '&bull;&bull;&bull;',
-      expiry: '&bull;&bull;/&bull;&bull;',
-      name: 'Full Name'
-    },
-   
-    // input masking aktifle
-    masks: {
-      cardNumber: false
-    },
-   
-    // valid/invalid CSS class bilgileri
-    classes: {
-      valid: 'jp-card-valid',
-      invalid: 'jp-card-invalid'
-    },
-   
-    // debug mode
-    debug: false
-    
-  });
+
+function myFunction1() {
+  let inputName = document.querySelector("#fname");
+  let inputEfternamen = document.querySelector("#lname");
+  if (inputName.value == "") {
+    prompt("fyll i namn");
+  } else {
+    console.log("Tack")
+    if (inputEfternamen.value == ""){
+      prompt("fyll i eftername");
+    }else{
+      console.log("Tack")
+    }
+  }
+}
+
+
+
