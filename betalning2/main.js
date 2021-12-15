af84a99c23a8dd68f462ff80c554da45b0ea9f3f
[Firat] <firat0705@hotmail.com>
2021-12-15 09:52:33 +0100

    andringar 10

--- betalning2/main.js
+++ betalning2/main.js
@@ -2,16 +2,38 @@
 function myFunction1() {
   let inputName = document.querySelector("#fname");
   let inputEfternamen = document.querySelector("#lname");
+  let inputPost = document.querySelector("#epost");
+  let inputPersonnummer = document.querySelector("#pnummer");
+  let inputLadress = document.querySelector("#adress");
   if (inputName.value == "") {
     prompt("fyll i namn");
   } else {
     console.log("Tack")
+  }
     if (inputEfternamen.value == ""){
       prompt("fyll i eftername");
     }else{
       console.log("Tack")
     }
+    if (inputPost.value == "") {
+      prompt("fyll i epost");
+    } else {
+      console.log("Tack!")
+    }
+  if (inputPersonnummer.value == "") {
+    prompt("fyll i personnummer");
+    
+  } else {
+    console.log("Tack!!")
   }
+
+  if (inputLadress.value == "") {
+    prompt("fyll i Levaransadressen");
+  } else {
+    console.log("tack")
+    
+  }
+  
 }
 
 
